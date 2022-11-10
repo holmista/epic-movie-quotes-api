23f73ff53ca04c1c1b3acab9e8a8f8b7692ba0ff
holmista <tornikebuchukuri@redberry.ge>
2022-11-10 13:18:59 +0400

    fix: add password in emails table

--- database/migrations/2014_10_11_212000_create_users_table.php
+++ database/migrations/2014_10_11_212000_create_users_table.php
@@ -15,8 +15,6 @@ return new class() extends Migration {
 		Schema::create('users', function (Blueprint $table) {
 			$table->id();
 			$table->string('name');
-			$table->timestamp('email_verified_at')->nullable();
-			$table->string('password')->nullable();
 			$table->string('google_id')->nullable();
 			$table->rememberToken();
 			$table->timestamps();
--- database/migrations/2022_11_09_211536_create_emails_table.php
+++ database/migrations/2022_11_09_211536_create_emails_table.php
@@ -15,6 +15,7 @@ return new class extends Migration {
 		Schema::create('emails', function (Blueprint $table) {
 			$table->id();
 			$table->string('email')->unique();
+			$table->string('password')->nullable();
 			$table->binary('is_primary');
 			$table->timestamp('email_verified_at')->nullable();
 			$table->unsignedBigInteger('user_id');
