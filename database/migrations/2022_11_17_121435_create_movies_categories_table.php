8b2350bf31819511e52cf98a848aca2d60ff446c
holmista <tornikebuchukuri@redberry.ge>
2022-11-17 16:31:51 +0400

    feat: create tables

---
+++ database/migrations/2022_11_17_121435_create_movies_categories_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('movies_categories', function (Blueprint $table) {
+			$table->id();
+			$table->foreignId('movie_id')->constrained('movies')->onDelete('cascade');
+			$table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('movies_categories');
+	}
+};
